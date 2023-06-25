f5d0b29bde029db53b303e13f590bf1da001b38b
Daniel Gonzalez <daniel.gonzalezmorales@veteransunited.com>
2023-06-25 09:44:26 -0500

    Trying to fix DI

---
+++ tests/TodoApi.FunctionalTests/TestDependencies.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using SolidToken.SpecFlow.DependencyInjection;
+using TodoApi.Application.Handlers;
+using TodoApi.Application.Interfaces;
+using TodoApi.Domain.Interfaces.Infrastructure;
+using TodoApi.Presentation.Controllers;
+using System;
+
+namespace TodoApi.FunctionalTests;
+
+public static class TestDependencies
+{
+    [ScenarioDependencies]
+    public static IServiceCollection CreateServices()
+    {
+        var services = new ServiceCollection();
+
+         // App Dependencies
+        services.AddSingleton<ITodoCommandHandler, TodoCommandHandler>();
+        services.AddSingleton<ITodoQueryHandler, TodoQueryHandler>();
+
+        // Infrastructure Dependencies
+        var mockTodoRespository = Substitute.For<ITodoRepository>();
+        services.AddSingleton(mockTodoRespository);
+
+        // Controllers
+        services.AddSingleton(provider => {
+           return new TodoController(Substitute.For<ILogger<TodoController>>(), provider.GetService<ITodoCommandHandler>(), provider.GetService<ITodoQueryHandler>());
+        });
+        
+        return services;
+    }
+
+}
